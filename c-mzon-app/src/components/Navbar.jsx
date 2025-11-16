b6288d17c1366be66a1c7b004121d1bb48a04659
komalkalsoom1 <komalchaudhary2705@gmail.com>
2025-11-16 16:26:31 +0500

    add

--- c-mzon-app/src/components/Navbar.jsx
+++ c-mzon-app/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { IoIosSave } from "react-icons/io";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { MdOutlineMessage } from "react-icons/md";
@@ -7,23 +7,45 @@ import { LuLayoutList } from "react-icons/lu";
 import { FaUserCircle } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <nav className="bg-[#1f999a] py-[6%] px-4
-    flex items-center justify-between">
+    <nav
+      className="bg-[#1f999a] py-[6%] px-4
+    flex items-center justify-between"
+    >
       {/* logo */}
-      <NavLink to='/'>
+      <Link to="/">
         <h1>MZONE</h1>
-      </NavLink>
+      </Link>
       {/* nav menulinks */}
-     <div>
-      <ul className="flex gap-6">
-      <li className="bg-[#f6f0f044] text-white rounded-full w-8 h-8 flex items-center justify-center"><NavLink><IoIosSave /></NavLink></li>
-      <li className=" rounded-full w-7 h-7"><NavLink><IoMdNotificationsOutline /></NavLink></li>
-      <li className="bg-[black] rounded-full w-7 h-7"><NavLink><MdOutlineMessage /></NavLink></li>
-      <li className="bg-[black] rounded-full w-7 h-7"><NavLink><LuLayoutList /></NavLink></li>
-      <li className="bg-[black] rounded-full w-7 h-7"><NavLink><FaUserCircle /></NavLink></li>
-      </ul>
-      <button className="bg-white px-3 py-1">Start Selling</button>
-     </div>
+      <div>
+        <ul className="flex gap-6">
+          <li className="bg-[#f6f0f044] text-white hover:bg-amber-400 hover:text-black rounded-full w-7 h-7 flex items-center justify-center">
+            <NavLink>
+              <IoIosSave />
+            </NavLink>
+          </li>
+          <li className=" rounded-full w-7 h-7">
+            <Link>
+              <IoMdNotificationsOutline />
+            </Link>
+          </li>
+          <li className="bg-[black] rounded-full w-7 h-7">
+            <Link>
+              <MdOutlineMessage />
+            </Link>
+          </li>
+          <li className="bg-[black] rounded-full w-7 h-7">
+            <Link>
+              <LuLayoutList />
+            </Link>
+          </li>
+          <li className="bg-[black] rounded-full w-7 h-7">
+            <Link>
+              <FaUserCircle />
+            </Link>
+          </li>
+        </ul>
+        <button className="bg-white px-3 py-1">Start Selling</button>
+      </div>
     </nav>
   );
 };
