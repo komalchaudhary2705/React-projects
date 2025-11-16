d99e4eff4a3ac2bbdb465ddbc0d36c0da516c544
komalkalsoom1 <komalchaudhary2705@gmail.com>
2025-11-16 17:36:43 +0500

    add

--- c-mzon-app/src/components/Sidear.jsx
+++ c-mzon-app/src/components/Sidear.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
 const Sidear = () => {
   return (
-    <div className='w-36 h-full p-5 border-2 border-gray-100 rounded'>Sidear</div>
-  )
-}
+    <div className="w-[200px] h-full p-5 border-2 border-gray-100 rounded">
+      {/* user profile */}
+      <div className="text-center flex flex-col items-center">
+        <FaUserCircle size={45} className="text-orange-500" />
+        <h1 className="font-semibold my-1">Jams Bond</h1>
+        <p className="text-[13px] pb-3 border-b-2 text-gray-500 border-gray-200">jamesbond007@gmail.com</p>
+      
+      </div>
+      {/* menu list */}
+    </div>
+  );
+};
 
-export default Sidear
+export default Sidear;
